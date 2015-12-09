bf9a19546b4e968b737b9529f859b00bfc4b720f
Kyunghyun <khpaeng@kaist.ac.kr>
2015-12-09 22:13:16 +0900

    Clean up

--- src/caffe/layers/seg_data_layer.cpp
+++ src/caffe/layers/seg_data_layer.cpp
@@ -172,7 +172,6 @@ void SegDataLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
           } else {
             top_data[top_index] = pixel * scale;
           }
-          //LOG(INFO) << top_data[top_index];
         }
       }
     }
@@ -187,7 +186,6 @@ void SegDataLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
         int top_index = ((item_id) * lh + h) * lw + w;
         Dtype pixel = static_cast<Dtype>(label_ptr[label_index++]);
         top_label[top_index] = pixel;
-        //if( top_label[top_index]>20 && top_label[top_index]!=255 ) LOG(INFO) << top_label[top_index];
       }
     }
     sample_cnt_++;
@@ -204,91 +202,3 @@ INSTANTIATE_CLASS(SegDataLayer);
 REGISTER_LAYER_CLASS(SegData);
 
 } // namespace caffe
-/*
-    for (int c=1; c<=num_class;c++) {
-      bool is_class = false;
-      cv::Mat cv_mask = cv::Mat::zeros( cv_label.rows, cv_label.cols, CV_8UC1);
-      for (int h = 0; h < cv_label.rows; ++h) {
-        const uchar* label_ptr = cv_label.ptr<uchar>(h);
-        const uchar* mask_ptr = cv_mask.ptr<uchar>(h);
-        int label_index = 0;
-        for (int w = 0; w < cv_label.cols; ++w) {
-          Dtype pixel = static_cast<Dtype>(label_ptr[label_index++]);
-          if(pixel==c) {
-            is_class = true;
-            cv_mask.at<uchar>(h,w) = 255;
-          }
-        }
-     }
-     if( is_class ) {
-      // blob detection in a binary label image
-      cv::Mat cv_cropped_img;
-      cv::Mat cv_cropped_label;
-      std::vector<std::vector<cv::Point> > contours;
-      cv::findContours( cv_mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE );
-      //Draw the contours
-      cv::Mat scale_map(cv_label.size(), CV_8UC1, cv::Scalar(0));
-      cv::Scalar colors[5];
-      colors[0] = cv::Scalar(128);  // scale 1 
-      colors[1] = cv::Scalar(128);  // scale 2
-      colors[2] = cv::Scalar(128);  // scale 3
-      colors[3] = cv::Scalar(128);  // scale 4
-      colors[4] = cv::Scalar(128);  // scale 5
-      for (size_t idx = 0; idx < contours.size(); idx++) {
-        double area = cv::contourArea(contours[idx], false);
-        cv::Rect bbox = cv::boundingRect(contours[idx]);
-        int nw = (bbox.width*10)/7;
-        int nh = (bbox.height*10)/7;
-        double obj_scale = area/(cv_label.rows*cv_label.cols);
-        LOG(INFO) << "scale: " << obj_scale;
-        LOG(INFO) << "pos_x: " << bbox.x + bbox.width/2;
-        LOG(INFO) << "pos_y: " << bbox.y + bbox.height/2;
-        LOG(INFO) << "new_w: " << nw;
-        LOG(INFO) << "new_h: " << nh;
-        int ih = cv_label.rows;
-        int iw = cv_label.cols;
-        int x1 = bbox.x + bbox.width/2 - nw/2;
-        int y1 = bbox.y + bbox.height/2- nh/2;
-        int x2 = bbox.x + bbox.width/2+ nw/2;
-        int y2 = bbox.y + bbox.height/2+ nh/2;
-	    // compute margin in case bbox is larger than image size.
-	    int margin_l_x = 0 - x1;
-	    int margin_t_y = 0 - y1;
-	    int margin_r_x = x2 - iw + 1;
-	    int margin_b_y = y2 - ih + 1;
-	    if ( margin_l_x > 0 || margin_t_y > 0 || margin_r_x > 0 || margin_b_y > 0 ) {
-		  if( x1 < 0 ) x1 = 0; else margin_l_x = 0;
-		  if( y1 < 0 ) y1 = 0; else margin_t_y = 0;
-		  if( x2 >= iw ) x2 = iw-1; else margin_r_x = 0;
-		  if( y2 >= ih ) y2 = ih-1; else margin_b_y = 0;
-		  cv::Rect roi(x1, y1, x2-x1+1, y2-y1+1);
-		  cv::Mat cv_sub_img = cv_image(roi);
-		  cv::Mat cv_sub_label = cv_label(roi);
-		  // mean padding
-		  cv::Scalar value = cv::Scalar( mean_values_[0], mean_values_[1], mean_values_[2] );
-		  cv::Scalar value_l = cv::Scalar( 255 );
-      	  cv::copyMakeBorder( cv_sub_img, cv_cropped_img, margin_t_y, margin_b_y, margin_l_x, margin_r_x, cv::BORDER_CONSTANT, value );
-      	  cv::copyMakeBorder( cv_sub_label, cv_cropped_label, margin_t_y, margin_b_y, margin_l_x, margin_r_x, cv::BORDER_CONSTANT, value_l );
-		  // warping
-    	  cv::resize(cv_cropped_img, cv_cropped_img, cv_crop_size, 0, 0, cv::INTER_LINEAR);
-    	  cv::resize(cv_cropped_label, cv_cropped_label, cv_crop_size, 0, 0, cv::INTER_LINEAR);
-	    } else { // if inner region in an image
-		  cv::Rect roi(x1, y1, x2-x1+1, y2-y1+1);
-		  cv_cropped_img = cv_image(roi);
-		  cv_cropped_label = cv_label(roi);
-    	  cv::resize(cv_cropped_img, cv_cropped_img, cv_crop_size, 0, 0, cv::INTER_LINEAR);
-    	  cv::resize(cv_cropped_label, cv_cropped_label, cv_crop_size, 0, 0, cv::INTER_LINEAR);
-        }
-    cv::Mat label_binary;
-    cv::threshold(cv_cropped_label, label_binary, 0.0, 255.0, cv::THRESH_BINARY);
-      cv::namedWindow("image",1);
-	  cv::imshow("image", cv_image);
-      cv::namedWindow("cimage",1);
-	  cv::imshow("cimage", cv_cropped_img);
-      cv::namedWindow("clabel",1);
-	  cv::imshow("clabel", label_binary);
-	  cv::waitKey(0);
-
-        cv::drawContours(scale_map, contours, idx, colors[0], CV_FILLED);
-        cv::rectangle(scale_map, bbox, cv::Scalar(255));
-      }*/
